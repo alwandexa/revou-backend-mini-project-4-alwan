5bacbaf5779a13842a0c4788b1cc7cf6c5b65aba
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-17 08:50:56 +0700

    feat(modules): Create Delete Movie Repository

--- src/repositories/movie-repository.ts
+++ src/repositories/movie-repository.ts
@@ -1,12 +1,27 @@
 import { ResultSetHeader } from "mysql2";
 
 import { pool } from "../lib/database";
-import { MovieModel } from "../models/movie-model";
+import { DeleteMovieRequest, MovieModel } from "../models/movie-model";
 
 const MovieRepository = {
   createMovie: (movieModel: MovieModel): Promise<number> => {
     return new Promise<number>((resolve, reject) => {
       const query = `INSERT INTO movies(title, director, release_date, runtime, movie_status) values('${movieModel.title}', '${movieModel.director}', '${movieModel.release_date}', ${movieModel.runtime}, '${movieModel.movie_status}')`;
+
+      pool.query<ResultSetHeader>(query, (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(rows.insertId);
+      });
+    });
+  },
+  deleteMovie: (deleteMovieRequest: DeleteMovieRequest): Promise<number> => {
+    console.log(deleteMovieRequest);
+    return new Promise<number>((resolve, reject) => {
+      const query = `UPDATE movies SET deleted_at = now() WHERE movie_id = ${deleteMovieRequest.id}`;
 
       pool.query<ResultSetHeader>(query, (err, rows) => {
         if (err) {
