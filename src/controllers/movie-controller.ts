74169cb26c908ea6ecbe144f7ad8cbf223266dca
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-17 08:51:45 +0700

    feat(modules): Create Delete Movie Controller

--- src/controllers/movie-controller.ts
+++ src/controllers/movie-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-import { CreateMovieRequest } from "../models/movie-model";
+import { CreateMovieRequest, DeleteMovieRequest } from "../models/movie-model";
 import { MovieService } from "../services/movie-service";
 
 const MovieController = {
@@ -11,8 +11,35 @@ const MovieController = {
         createMovieRequest
       );
 
-      res.status(200).json({
+      res.status(201).json({
+        success: true,
         data: createMovieResponse,
+        message: "successfully created",
+      });
+    } catch (error) {
+      let errorMessage = "server error";
+
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+
+      res.status(500).json({
+        success: true,
+        error: errorMessage,
+      });
+    }
+  },
+  deleteMovie: async (req: Request, res: Response) => {
+    try {
+      const deleteMovieRequest = req.body as DeleteMovieRequest;
+      const deleteMovieResponse = await MovieService.deleteMovie(
+        deleteMovieRequest
+      );
+
+      res.status(200).json({
+        success: true,
+        data: deleteMovieResponse,
+        message: "successfully deleted",
       });
     } catch (error) {
       let errorMessage = "server error";
