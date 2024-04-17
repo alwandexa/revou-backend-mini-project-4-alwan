c4d9204f947839e9692c00e9ccb7011859a4b1c4
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-17 07:14:13 +0700

    feat(function): Create Server File

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { MovieController } from "./controllers/movie-controller";
+
+const startServer = async () => {
+  try {
+    const app = express();
+    app.use(express.json());
+
+    const movieRouter = express.Router();
+    movieRouter.post("/movie/add", MovieController.createMovie);
+
+    app.use(movieRouter);
+
+    const PORT = 3001;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running in http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("failed to start server :", error);
+    process.exit(1);
+  }
+};
+
+startServer();
