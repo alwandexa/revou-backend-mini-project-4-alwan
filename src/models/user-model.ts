834de6c1e0bdc75f7e849c3f6a408b78fcdd1229
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-19 05:55:01 +0700

    feat(modules): Create Create User Request Model

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,7 +1,17 @@
 export interface UserModel {
-    user_id : number
-    name : string
-    email : string
-    password : string
-    birthdate : Date
+  user_id: number;
+  name: string;
+  email: string;
+  password: string;
+  birthdate: Date;
+}
+
+export interface CreateUserRequest {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export interface CreateUserResponse {
+  user_id: number;
 }
