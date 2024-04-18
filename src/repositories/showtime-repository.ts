9f070fbaf774cf8071bed97011f2d39b99c2880f
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-18 11:28:01 +0700

    feat(modules): Create Update Showtime Repository

--- src/repositories/showtime-repository.ts
+++ src/repositories/showtime-repository.ts
@@ -1,4 +1,4 @@
-import { ResultSetHeader } from "mysql2";
+import { PoolConnection, ResultSetHeader } from "mysql2";
 
 import { pool } from "../lib/database";
 import {
@@ -12,7 +12,7 @@ const ShowtimeRepository = {
     createShowtimeRequest: CreateShowtimeRequest
   ): Promise<number> => {
     return new Promise<number>((resolve, reject) => {
-      let query = `INSERT INTO showtimes(movie_id, showtime) VALUES`;
+      let query = `INSERT INTO showtimes(movie_id, showtime) VALUES as`;
 
       let data = ``;
 
@@ -49,6 +49,96 @@ const ShowtimeRepository = {
         resolve({
           movie_id: rows.insertId,
         });
+      });
+    });
+  },
+  updateShowtime: (): Promise<number> => {
+    return new Promise<number>((resolve, reject) => {
+      pool.getConnection((error, connection) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        connection.beginTransaction((err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          connection.query(
+            "DELETE FROM showtimes WHERE movie_id=1 ",
+            (err, result) => {
+              if (err) {
+                connection.rollback(() => {
+                  reject(err);
+                });
+                return;
+              }
+
+              connection.query(
+                "INSERT INTO showtimes(movie_id, showtime) VALUES(1, '18:30' xx)",
+                (err, result) => {
+                  if (err) {
+                    reject(err);
+                    connection.rollback(() => {
+                      return;
+                    });
+                  }
+                  connection.commit((err) => {
+                    if (err) {
+                      connection.rollback(() => {
+                        reject(err);
+                        return;
+                      });
+                    }
+                    connection.release();
+                    resolve(1);
+                  });
+                }
+              );
+            }
+          );
+        });
+      });
+    });
+  },
+  updateShowtime2: async (): Promise<number> => {
+    return new Promise<number>((resolve, reject) => {
+      pool.getConnection((error, connection) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        try {
+          connection.beginTransaction(async (err) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+          });
+
+          connection.query("DELETE FROM showtimes WHERE movie_id=1 xxx", 
+          );
+          connection.query(
+            "INSERT INTO showtimes(movie_id, showtime) VALUES(1, '18:30')"
+          );
+
+          connection.commit();
+        } catch (err) {
+          connection.rollback((err) => {
+            if (err) {
+              connection.rollback(() => {
+                reject(err);
+                return;
+              });
+            }
+          });
+          console.error("Error occurred during the transaction:", err);
+        } finally {
+          if (connection) {
+            connection.release();
+          }
+        }
       });
     });
   },
