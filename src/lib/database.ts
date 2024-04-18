9a18af5cea6b87dde9d4cb21326080b629f2ad02
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-18 14:25:44 +0700

    ref(modules): Change to mysql2/promise

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,27 +1,6 @@
-import mysql from "mysql2";
+import mysql, { Pool } from "mysql2/promise";
 
-// export const mysqlConnection = () => {
-//   return new Promise<mysql.Connection>((resolve, reject) => {
-//     const connection = mysql.createConnection({
-//       host: "localhost",
-//       port: 3306,
-//       user: "root",
-//       password: "root",
-//       database: "revou",
-//     });
-
-//     connection.connect((error) => {
-//       if (error) {
-//         reject(error);
-//         return;
-//       }
-
-//       resolve(connection);
-//     });
-//   });
-// };
-
-export const pool = mysql.createPool({
+export const pool: Pool = mysql.createPool({
   connectionLimit: 10,
   host: "localhost",
   port: 3306,
