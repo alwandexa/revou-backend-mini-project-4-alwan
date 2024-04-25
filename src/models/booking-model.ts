7429bb4d73661f71d4e48f76983fc93b15d08673
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-25 16:22:11 +0700

    feat(modules): Create Booking Modeel

--- src/models/booking-model.ts
+++ src/models/booking-model.ts
@@ -1,3 +1,11 @@
 export interface BookingModel {
-    
+  booking_id: number;
+  user_id: number;
+  schedule_id: number;
 }
+
+export interface CreateBookingRequest
+  extends Omit<BookingModel, "booking_id"> {}
+
+export interface CreateBookingResponse
+  extends Pick<BookingModel, "booking_id"> {}
