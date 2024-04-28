876103b13adf51cd0e980a386677c8d8f0aa2e3c
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-28 17:23:25 +0700

    fix(functions): Fix endpoint method

--- src/routes/movie-router.ts
+++ src/routes/movie-router.ts
@@ -7,7 +7,7 @@ const movieRouter = Router();
 movieRouter.post("/movie/add", auth("admin"), MovieController.createMovie);
 movieRouter.delete("/movie/delete", auth("admin"), MovieController.deleteMovie);
 movieRouter.patch("/movie/update", auth("admin"), MovieController.updateMovie);
-movieRouter.get("/movie", MovieController.getMovieById);
+movieRouter.post("/movie", MovieController.getMovieById);
 movieRouter.get("/movie/list", MovieController.getAllMovies);
 
 export default movieRouter;
