0c9e9c117904bdda159fb7aea493be450e3c3b08
alwandexa <alwan.wirawan@djembatandua.com>
2024-05-01 14:01:12 +0700

    feat(functions): Add validation when booking

--- src/services/booking-service.ts
+++ src/services/booking-service.ts
@@ -7,6 +7,8 @@ import {
 } from "../models/booking-model";
 import { BookingRepository } from "../repositories/booking-repository";
 import { validateRequiredKeys } from "../utils/util";
+import { ScheduleRepository } from "../repositories/schedule-repository";
+import { StudioRepository } from "../repositories/studio-repository";
 
 const BookingService = {
   create: async (
@@ -18,6 +20,26 @@ const BookingService = {
       "schedule_id",
       "amount",
     ]);
+
+    const schedule = await ScheduleRepository.lockSchedule(
+      createBookingRequest.schedule_id,
+      connection
+    );
+
+    const scheduleBookingAmount =
+      await BookingRepository.getScheduleBookingAmount(
+        createBookingRequest.schedule_id,
+        connection
+      );
+
+    const studio = await StudioRepository.getStudioById(
+      schedule.studio_id,
+      connection
+    );
+
+    if (scheduleBookingAmount + createBookingRequest.amount > studio.capacity) {
+      throw new Error("Schedule is full");
+    }
 
     const createdBookingId = await BookingRepository.create(
       createBookingRequest,
